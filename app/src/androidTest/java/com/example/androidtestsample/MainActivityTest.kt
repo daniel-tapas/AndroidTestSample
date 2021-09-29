5d295e331bb8c7ff61098d1c4d89c91d31606a96
daniel-tapas <daniel.k@tapasmedia.co>
2021-09-29 14:46:39 +0900

    Update activity_main.xml

--- app/src/androidTest/java/com/example/androidtestsample/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/androidtestsample/MainActivityTest.kt
@@ -2,7 +2,6 @@ package com.example.androidtestsample
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -22,18 +21,17 @@ internal class MainActivityTest {
     fun button_has_hello_world_text() {
         // View 지정
         // ID 로 찾기
-        onView(withId(R.id.button))
+        onView(withId(R.id.up))
             .perform(click()) // 클릭 실행
-            .check(matches(withText("Hello, world!"))) // 뷰 확인
+            .check(matches(withText("UP"))) // 뷰 확인
 
         // Text 로 찾기
-        onView(withText("Hello, world!"))
+        onView(withText("UP"))
     }
 
     @Test
-    fun editText_change_tapas_to_hello() {
-        onView(withId(R.id.editText))
-            .perform(replaceText("Hello")) // text 입력
-            .check(matches(withText("Hello")))
+    fun textview_shown_tapas_text() {
+        onView(withId(R.id.counter))
+            .check(matches(withText("0")))
     }
 }
